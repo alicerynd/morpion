e574803aacf35f99f0f62c42356d97fe80b18b85
alicerynd <raynaud.alice@yahoo.fr>
2019-11-23 15:10:33 +0100

    Added project

---
+++ morpion/Morpion.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace morpion
+{
+    partial class Morpion
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.message = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonValidate = new System.Windows.Forms.Button();
+            this.input_player1 = new System.Windows.Forms.TextBox();
+            this.input_player2 = new System.Windows.Forms.TextBox();
+            this.retry_button = new System.Windows.Forms.Button();
+            this.player2_score_input = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.player1_score_input = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(456, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 0;
+            this.button1.TabStop = false;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.morpionClick);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(562, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 100);
+            this.button2.TabIndex = 1;
+            this.button2.TabStop = false;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.morpionClick);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Comic Sans MS", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(668, 141);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 100);
+            this.button3.TabIndex = 2;
+            this.button3.TabStop = false;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.morpionClick);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Comic Sans MS", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(456, 247);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 100);
+            this.button4.TabIndex = 5;
+            this.button4.TabStop = false;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.morpionClick);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Comic Sans MS", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(562, 247);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 100);
+            this.button5.TabIndex = 4;
+            this.button5.TabStop = false;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.morpionClick);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Comic Sans MS", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(668, 247);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 100);
+            this.button6.TabIndex = 3;
+            this.button6.TabStop = false;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.morpionClick);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Comic Sans MS", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(456, 353);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 100);
+            this.button7.TabIndex = 8;
+            this.button7.TabStop = false;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.morpionClick);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Comic Sans MS", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(562, 353);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 100);
+            this.button8.TabIndex = 7;
+            this.button8.TabStop = false;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.morpionClick);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Comic Sans MS", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(668, 353);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 100);
+            this.button9.TabIndex = 6;
+            this.button9.TabStop = false;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.morpionClick);
+            // 
+            // message
+            // 
+            this.message.Font = new System.Drawing.Font("Verdana", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.message.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.message.Location = new System.Drawing.Point(451, 56);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(351, 25);
+            this.message.TabIndex = 9;
+            this.message.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Nom du joueur 1 :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(42, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(223, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Nom du joueur 2 :";
+            // 
+            // buttonValidate
+            // 
+            this.buttonValidate.Font = new System.Drawing.Font("Verdana", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonValidate.Location = new System.Drawing.Point(47, 247);
+            this.buttonValidate.Name = "buttonValidate";
+            this.buttonValidate.Size = new System.Drawing.Size(181, 42);
+            this.buttonValidate.TabIndex = 12;
+            this.buttonValidate.Text = "Jouer !";
+            this.buttonValidate.UseVisualStyleBackColor = true;
+            this.buttonValidate.Click += new System.EventHandler(this.buttonValidate_Click);
+            // 
+            // input_player1
+            // 
+            this.input_player1.Location = new System.Drawing.Point(47, 85);
+            this.input_player1.MaxLength = 8;
+            this.input_player1.Name = "input_player1";
+            this.input_player1.Size = new System.Drawing.Size(342, 31);
+            this.input_player1.TabIndex = 13;
+            // 
+            // input_player2
+            // 
+            this.input_player2.Location = new System.Drawing.Point(47, 181);
+            this.input_player2.MaxLength = 8;
+            this.input_player2.Name = "input_player2";
+            this.input_player2.Size = new System.Drawing.Size(342, 31);
+            this.input_player2.TabIndex = 14;
+            // 
+            // retry_button
+            // 
+            this.retry_button.Font = new System.Drawing.Font("Verdana", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.retry_button.Location = new System.Drawing.Point(587, 476);
+            this.retry_button.Name = "retry_button";
+            this.retry_button.Size = new System.Drawing.Size(181, 42);
+            this.retry_button.TabIndex = 15;
+            this.retry_button.Text = "Rejouer";
+            this.retry_button.UseVisualStyleBackColor = true;
+            this.retry_button.Visible = false;
+            this.retry_button.Click += new System.EventHandler(this.retry_button_Click);
+            // 
+            // player2_score_input
+            // 
+            this.player2_score_input.Font = new System.Drawing.Font("Verdana", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player2_score_input.Location = new System.Drawing.Point(28, 107);
+            this.player2_score_input.Name = "player2_score_input";
+            this.player2_score_input.Size = new System.Drawing.Size(230, 30);
+            this.player2_score_input.TabIndex = 16;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.player2_score_input);
+            this.groupBox1.Controls.Add(this.player1_score_input);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(47, 340);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(342, 178);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Scores";
+            // 
+            // player1_score_input
+            // 
+            this.player1_score_input.Font = new System.Drawing.Font("Verdana", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player1_score_input.Location = new System.Drawing.Point(28, 53);
+            this.player1_score_input.Name = "player1_score_input";
+            this.player1_score_input.Size = new System.Drawing.Size(230, 30);
+            this.player1_score_input.TabIndex = 17;
+            // 
+            // Morpion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 565);
+            this.Controls.Add(this.retry_button);
+            this.Controls.Add(this.input_player2);
+            this.Controls.Add(this.input_player1);
+            this.Controls.Add(this.buttonValidate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Morpion";
+            this.Text = "Jouer à morpion !";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label message;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonValidate;
+        private System.Windows.Forms.TextBox input_player1;
+        private System.Windows.Forms.TextBox input_player2;
+        private System.Windows.Forms.Button retry_button;
+        private System.Windows.Forms.Label player2_score_input;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label player1_score_input;
+    }
+}
+
---
+++ morpion/Morpion.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace morpion
+{
+    public partial class Morpion : Form
+    {
+        private bool gameStarted = false;
+        private string turn;
+        private string player1;
+        private string player2;
+        private int player1_score = 0;
+        private int player2_score = 0;
+        private string playerWon;
+
+        public Morpion()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonValidate_Click(object sender, EventArgs e)
+        {
+            if (input_player1.Text != "" && input_player2.Text != "")
+            {
+                player1 = input_player1.Text;
+                player2 = input_player2.Text;
+                input_player1.BackColor = Color.White;
+                input_player2.BackColor = Color.White;
+                input_player1.Enabled = false;
+                input_player2.Enabled = false;
+                buttonValidate.Enabled = false;
+                buttonValidate.Text = "C'est parti !";
+
+                player1_score_input.Text = player1 + " : " + player1_score;
+                player2_score_input.Text = player2 + " : " + player2_score;
+
+                gameStarted = true;
+                turn = player1;
+                message.Visible = true;
+                message.Text = "C'est au tour de " + turn;
+            }
+            else
+            {
+                input_player1.BackColor = Color.Red;
+                input_player2.BackColor = Color.Red;
+            }
+        }
+
+        private void morpionClick(object sender, EventArgs en)
+        {
+            if (gameStarted)
+            {
+                Button morpionButton = sender as Button;
+
+                if (turn == player1)
+                {
+                    morpionButton.Text = "X";
+                    turn = player2;
+                }
+                else
+                {
+                    morpionButton.Text = "O";
+                    turn = player1;
+                }
+
+                morpionButton.Enabled = false;
+                message.Text = "C'est au tour de " + turn;
+
+                if (someoneWin())
+                {
+                    if (turn == player1)
+                    {
+                        playerWon = player2;
+                        message.Text = playerWon + " a gagné !";
+                        player2_score = player2_score + 1;
+                    }
+                        
+                    else
+                    {
+                        playerWon = player1;
+                        message.Text = playerWon + " a gagné !";
+                        player1_score = player1_score + 1;
+                    }
+
+                    player1_score_input.Text = player1 + " : " + player1_score;
+                    player2_score_input.Text = player2 + " : " + player2_score;
+
+                    retry_button.Visible = true;
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                    button4.Enabled = false;
+                    button5.Enabled = false;
+                    button6.Enabled = false;
+                    button7.Enabled = false;
+                    button8.Enabled = false;
+                    button9.Enabled = false;
+                }
+
+                if (draw())
+                {
+                    playerWon = player1;
+                    message.Text = "Match nul !";
+                    retry_button.Visible = true;
+
+                    player1_score_input.Text = player1 + " : " + player1_score;
+                    player2_score_input.Text = player2 + " : " + player2_score;
+                }
+            }
+        }
+
+        private bool someoneWin()
+        {
+            // lignes horizontales
+            if (button1.Text == button2.Text && button2.Text == button3.Text && button2.Text != "")
+                return true;
+            if (button4.Text == button5.Text && button5.Text == button6.Text && button5.Text != "")
+                return true;
+            if (button7.Text == button8.Text && button8.Text == button9.Text && button8.Text != "")
+                return true;
+            // lignes verticales
+            if (button1.Text == button4.Text && button4.Text == button7.Text && button4.Text != "")
+                return true;
+            if (button2.Text == button5.Text && button5.Text == button8.Text && button5.Text != "")
+                return true;
+            if (button3.Text == button6.Text && button6.Text == button9.Text && button6.Text != "")
+                return true;
+            // diagonales
+            if (button1.Text == button5.Text && button5.Text == button9.Text && button5.Text != "")
+                return true;
+            if (button3.Text == button5.Text && button5.Text == button7.Text && button5.Text != "")
+                return true;
+
+            return false;
+
+        }
+
+        private bool draw()
+        {
+            if (button1.Text != "" && button2.Text != "" && button3.Text != "" && button4.Text != "" && button5.Text != "" && button6.Text != "" && button7.Text != "" && button8.Text != "" && button9.Text != "" && someoneWin() == false)
+                return true;
+
+            return false;
+        }
+
+        private void retry_button_Click(object sender, EventArgs e)
+        {
+
+            turn = playerWon;
+            message.Text = "C'est au tour de " + turn;
+
+            button1.Enabled = true;
+            button1.Text = "";
+
+            button2.Enabled = true;
+            button2.Text = "";
+
+            button3.Enabled = true;
+            button3.Text = "";
+
+            button4.Enabled = true;
+            button4.Text = "";
+
+            button5.Enabled = true;
+            button5.Text = "";
+
+            button6.Enabled = true;
+            button6.Text = "";
+
+            button7.Enabled = true;
+            button7.Text = "";
+
+            button8.Enabled = true;
+            button8.Text = "";
+
+            button9.Enabled = true;
+            button9.Text = "";
+        }
+    }
+}
